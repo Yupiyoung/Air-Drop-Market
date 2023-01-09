9b2ed7f793a44be98f70a6b44b3dbb438c6fd9be
Max <Makyzs1050@gmail.com>
2023-01-10 02:29:43 +0300

    add home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './pages/Home';
 
 function App() {
+  const [mobileBottom, setMobileBottom] = React.useState(false)
+  const resizeComp = () => {
+    if (window.innerWidth <= 425) {
+      setMobileBottom(true);
+    } else {
+      setMobileBottom(false);
+    }
+  }
+  React.useEffect(() => {
+    window.addEventListener('resize', resizeComp);
+    resizeComp();
+    return () => {
+      window.removeEventListener('resize', resizeComp);
+    };
+  }, []);
   return (
-    <div className="App">
+    <div className="App" style={mobileBottom ? {marginBottom: '100px'} : null}>
       <div className='container'>
         <Header></Header>
       </div>

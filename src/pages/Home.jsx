0ef60a7a1e7045d826d5bdbe4f3488161ea6db8f
Max <Makyzs1050@gmail.com>
2023-01-13 03:13:49 +0300

    add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,49 +7,43 @@ import Slide1 from './Slide1.jpg';
 import Slide2 from './Slide2.jpg';
 import Slide3 from './Slide3.jpg';
 import Sneakers1 from './Sneakers1.png';
+import Expand_left from './Expand_left.svg';
+import Expand_right from './Expand_right.svg';
 
 function Home() {
   const [brandsScroll, setBrandsScroll] = React.useState(0);
+
   const activeSlideRef = React.useRef(null);
+  const clickRightButtonScroll = (value) => {
+    const actualLeft = changeBrandScroll();
+    activeSlideRef.current.scrollTo(actualLeft + value, 0);
+  };
+  const clickLeftButtonScroll = (value) => {
+    const actualLeft = changeBrandScroll();
+    if (actualLeft <= 0) {
+      activeSlideRef.current.scrollTo(0, 0);
+    } else {
+      activeSlideRef.current.scrollTo(actualLeft - value, 0);
+    }
+  };
   const changeBrandScroll = () => {
-    const width = activeSlideRef.current.scrollWidth - 364;
-    const object = 110;
+    const width = activeSlideRef.current.scrollWidth;
+    const object = 150;
     const total = ~~(width / object);
-    console.log(total - ~~((width - activeSlideRef.current.scrollLeft) / object));
+    setBrandsScroll(total - ~~((width - activeSlideRef.current.scrollLeft) / object));
+    return activeSlideRef.current.scrollLeft;
   };
   React.useEffect(() => {
     activeSlideRef.current.addEventListener('scroll', changeBrandScroll);
     return () => activeSlideRef.current.removeEventListener('scroll', changeBrandScroll);
   }, []);
-  // const resizeComp = () => {
-  //   if (window.innerWidth <= 992) {
-  //     setSlider(4);
-  //     setSliderSpace(500);
-  //   }
-  //   if (window.innerWidth <= 768) {
-  //     setSlider(3);
-  //     setSliderSpace(400);
-  //   }
-  //   if (window.innerWidth <= 768) {
-  //     setSlider(2);
-  //     setSliderSpace(370);
-  //   } else {
-  //     setSlider(3);
-  //   }
-  // };
-  // React.useEffect(() => {
-  //   window.addEventListener('resize', resizeComp);
-  //   resizeComp();
-  //   return () => {
-  //     window.removeEventListener('resize', resizeComp);
-  //   };
-  // }, []);
+
   return (
     <>
       <style>
         {`.swiper-slide-active{
           filter: drop-shadow(0px 0px 12px rgba(0, 0, 0, 0.1));
-          transition: filter 0.5s linear;
+          transition: filter 1s linear;
           }
           .swiper-scrollbar.swiper-scrollbar-horizontal{    
             border-radius: 10px;
@@ -85,11 +79,11 @@ function Home() {
             <h2>Популярные товары</h2>
             <p>Показать все</p>
           </div>
-          <div className={homeStyle.popularProductBlockNew}>
+          <div className={homeStyle.popularProductBlock}>
             <Swiper slidesPerView={1} modules={[Scrollbar]} scrollbar={{ draggable: true }}>
               {[Sneakers1, Sneakers1, Sneakers1, Sneakers1, Sneakers1].map((element, index) => (
                 <SwiperSlide key={`slide${index}`}>
-                  <div className={homeStyle.popularProductCardNew}>
+                  <div className={homeStyle.popularProductCard}>
                     <img alt="Sneakers1" src={element}></img>
                     <h3>Nike Dunk Low Halloween</h3>
                   </div>
@@ -97,31 +91,36 @@ function Home() {
               ))}
             </Swiper>
           </div>
-          {/* <div className={homeStyle.popularProductBlock}>
-            {[1, 2, 3, 4, 5, 6, 7, 8].map((element, index) => (
-              <div className={homeStyle.popularProductCard}>
-                <img alt="Sneakers1" src={Sneakers1}></img>
-                <h3>Nike Dunk Low Halloween</h3>
-              </div>
-            ))}
-          </div> */}
         </div>
         <div className={homeStyle.brandIcons}>
           <div className={homeStyle.brandIconsName}>
             <h2>Бренды</h2>
           </div>
-          <div className={homeStyle.brandIconsWrap} ref={activeSlideRef}>
-            {[0, 1, 2, 3, 4].map((element, index) => (
-              <div
-                onClick={() => console.log(index)}
-                className={
-                  brandsScroll === index ? homeStyle.brandIconCardActive : homeStyle.brandIconCard
-                }>
-                <img
-                  alt="Sneakers1"
-                  src="https://cdn-icons-png.flaticon.com/512/732/732084.png"></img>
-              </div>
-            ))}
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <img
+              alt="Expand_left"
+              height="20px"
+              src={Expand_left}
+              onClick={() => clickLeftButtonScroll(150)}></img>
+            <div className={homeStyle.brandIconsWrap} ref={activeSlideRef}>
+              {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((element, index) => (
+                <div
+                  key={`iconBrand${index}`}
+                  onClick={() => console.log(index)}
+                  className={
+                    brandsScroll === index ? homeStyle.brandIconCardActive : homeStyle.brandIconCard
+                  }>
+                  <img
+                    alt="Sneakers1"
+                    src="https://cdn-icons-png.flaticon.com/512/732/732084.png"></img>
+                </div>
+              ))}
+            </div>
+            <img
+              alt="Expand_right"
+              height="20px"
+              src={Expand_right}
+              onClick={() => clickRightButtonScroll(150)}></img>
           </div>
         </div>
         <div className={homeStyle.brand}>
@@ -132,7 +131,7 @@ function Home() {
           <div className={homeStyle.brandProductBlockWrap}>
             <div className={homeStyle.brandProductBlock}>
               {[1, 2, 3, 4, 5, 6, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((element, index) => (
-                <div className={homeStyle.brandProductCard}>
+                <div className={homeStyle.brandProductCard} key={`brandProductBlock1${index}`}>
                   <img alt="Sneakers1" src={Sneakers1}></img>
                 </div>
               ))}
@@ -147,7 +146,7 @@ function Home() {
           <div className={homeStyle.brandProductBlockWrap}>
             <div className={homeStyle.brandProductBlock}>
               {[1, 2, 3, 4, 5, 6, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((element, index) => (
-                <div className={homeStyle.brandProductCard}>
+                <div className={homeStyle.brandProductCard} key={`brandProductBlock2${index}`}>
                   <img alt="Sneakers1" src={Sneakers1}></img>
                 </div>
               ))}
